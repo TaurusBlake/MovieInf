6d423cd34aac758d3d85fce21da0af6df3d51720
TaurusBlake <shamgarxie@gmail.com>
2024-09-08 21:03:36 +0800

    create movieInf_BS(crawler by BeautifulSoup) & add beautifulSoup requests to onepage_crawler.py

---
+++ MovieInf_BS.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.vscinemas.com.tw/vsweb/film/"
+web = requests.get(url+"index.aspx")
+soup = BeautifulSoup(web.text,"lxml")
+
+
+try:
+    print("熱售中讀取成功")
+    # 抓取頁數，找到pagebar中的所有li
+    page = soup.find("section",class_="pagebar")
+    page_amount = page.find_all("li")
+    #印出按鈕數量確認
+    # print(len(page_amount))
+    try:
+        links = []
+        # 利用迴圈抓取連結，並丟入links空陣列
+        for pages in range(len(page_amount)-2):
+            # 使用 find_all 定位 到 class infoArea 標籤
+            # 先抓取確定不會超過數量的位置，以便確認電影總數
+            movie_amount = soup.find_all("section", class_="infoArea")
+            # print(len(movie_amount))
+            #利用迴圈抓取每部電影詳細介紹的連結，連結存於a的href中
+            for movies in movie_amount:
+                link = movies.find("a")["href"]
+                links.append("https://www.vscinemas.com.tw/vsweb/film/"+link)
+            #因為目前只有3頁，只需切換2次(目前此頁有5個按鈕，代表只有3頁，每次切換從第2頁開始)
+            if pages+2 < len(page_amount)-1:
+                #它切按鈕會出錯因為網址會改變，改為使用網址的規律來切換頁面
+                web=requests.get(url+"index.aspx?p=%d" %(pages+2))
+                soup = BeautifulSoup(web.text,"lxml")
+    
+        try:
+            web=requests.get(url+"coming.aspx")
+            soup = BeautifulSoup(web.text,"lxml")
+            print("即將上映讀取成功")
+            # 利用迴圈抓取連結，並丟入links空陣列
+            for pages in range(len(page_amount)-2):
+                # 使用 find_all 定位 到 class infoArea 標籤
+                # 先抓取確定不會超過數量的位置，以便確認電影總數
+                movie_amount = soup.find_all("section", class_="infoArea")
+                # print(len(movie_amount))
+                #利用迴圈抓取每部電影詳細介紹的連結，連結存於a的href中
+                for movies in movie_amount:
+                    link = movies.find("a")["href"]
+                    links.append("https://www.vscinemas.com.tw/vsweb/film/"+link)
+                #因為目前只有3頁，只需切換2次(目前此頁有5個按鈕，代表只有3頁，每次切換從第2頁開始)
+                if pages+2 < len(page_amount)-1:
+                    #它切按鈕會出錯因為網址會改變，改為使用網址的規律來切換頁面
+                    web=requests.get(url+"coming.aspx?p=%d" %(pages+2))
+                    soup = BeautifulSoup(web.text,"lxml")
+        except Exception as e:
+            print("威秀-即將上映連結抓取失敗", e)
+    except Exception as e:
+        print("威秀-熱售中連結抓取失敗", e)
+except Exception as e:
+    print("網頁讀取失敗", e)
--- onepage_crawler.py
+++ onepage_crawler.py
@@ -1,22 +1,31 @@
+from bf4 import Beautifulsoup
+import requset
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# 初始化 WebDriver（确保你安装了相应的驱动程序，如 ChromeDriver）
-driver = webdriver.Chrome()
+
+# 創建 Chrome 瀏覽器選項，不開啟畫面執行爬蟲
+options = Options()
+options.add_argument("--headless")  # 設定為無頭模式
+options.add_argument("--disable-gpu")  # 禁用 GPU
+#使用selenium開啟chrome driver
+driver = webdriver.Chrome(options=options)
+
+url = "https://www.vscinemas.com.tw/vsweb/film/detail.aspx?id=7258"
+web = requset.get(url)
+soup = Beautifulsoup(web, "lxml")
 
 # 打开目标页面
-driver.get("https://www.vscinemas.com.tw/vsweb/film/detail.aspx?id=7223")
-
+driver.get(url)
 # 找到目标元素（比如通过 CSS 选择器）
-element = driver.find_element(By.XPATH, "/html/body/article/section/div[2]/section/div[1]/span[1]")
-
-# 使用 JavaScript 获取伪元素的内容
-pseudo_element_content = driver.execute_script(
+rating_url = driver.find_element(By.XPATH, "/html/body/article/section/div[2]/section/div[1]/span[1]")
+# 使用 JavaScript 获取伪元素的内容(影片分級)
+rating_text = driver.execute_script(
     "return window.getComputedStyle(arguments[0], '::before').getPropertyValue('content');",
-    element
-)
+    rating_url)
 
-print(pseudo_element_content)
+print(rating_text)
 
 # 关闭浏览器
 driver.quit()
